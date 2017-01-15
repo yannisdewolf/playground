b839fb488d67d8663fd21b6ac7ce6502cd10bd0b
Yannis De Wolf <yannisdewolf@gmail.com>
2017-01-15 18:02:40 +0100

    different db for testing

--- src/main/java/be/dewolf/DbappApplication.java
+++ src/main/java/be/dewolf/DbappApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DbappApplication {
+public class DbappApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbappApplication.class, args);
 	}
+
 }
--- src/test/java/be/dewolf/ApplicationIT.java
+++ src/test/java/be/dewolf/ApplicationIT.java
@@ -9,11 +9,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -23,8 +24,8 @@ import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = DbappApplication.class)
-@WebAppConfiguration
 @Transactional
+@TestPropertySource(locations="classpath:test.properties")
 public class ApplicationIT {
 
     @Autowired
@@ -79,9 +80,18 @@ public class ApplicationIT {
     }
 
     @Test
+    public void verifiesGreetingpageLoads() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/greeting"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
     public void verifiesHomepageLoads() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+        .andDo(MockMvcResultHandlers.print());
+
 
     }
+
 }
